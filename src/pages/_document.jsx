8e810c2284569a934fa93f24846b749d2caa5fb4
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2023-11-15 20:52:12 +0300

    add some miss file

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <link rel='icon' href='img/favicon.png' />
+        <link rel='apple-touch-icon' href='img/apple-touch-icon.png' />
+        <link rel='manifest' href='/manifest.webmanifest' />
+      </Head>
       <body>
         <Main />
         <NextScript />
